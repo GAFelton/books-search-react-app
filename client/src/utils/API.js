e9a4607821c214b756dfaa04083ac179e0a8ae65
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-18 15:02:34 -0800

    Moved web api requests to backend.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const apiKey = process.env.BOOKS_API_Key;
 
 // The getBooks method retrieves books from the server
 // It accepts a "query" or term to search the book api for
