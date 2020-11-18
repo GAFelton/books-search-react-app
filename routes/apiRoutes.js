f055010f77811b31b5877efd20f90f6b36091a01
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-18 15:03:01 -0800

    Separated dbRoutes from web apiRoutes, exporting via index.js in routes directory.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
-const db = require("../models");
+const axios = require("axios");
+const apiKey = process.env.BOOKS_API_KEY;
 
-router.get("/books", (req, res) => {
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
-  db.Book.find({
-    title: { $regex: new RegExp(req.query.q, 'i')}
-  })
-    .then(books => res.json(books))
-    .catch(err => res.status(422).json(err).end());
-});
+
+router.get("/web/book/:id", async (req, res, next) => {
+  try {
+    const query = req.params.id;
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`);
+    console.log(response);
+    res.json(response.data);
+  }
+  catch (err) {
+    console.error("Error in API Call", err);
+  }
+})
 
 module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import dbRoutes from "./dbRoutes";
+import apiRoutes from "./apiRoutes";
+
+const dataRoutes = {
+  dbRoutes,
+  apiRoutes,
+};
+
+export default dataRoutes;
