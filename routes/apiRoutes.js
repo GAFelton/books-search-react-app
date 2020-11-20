63d5d286a955fbb86781df5a31a46bc46d9e72a9
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-20 14:19:41 -0800

    Adjusted book search route, added selfLink route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,21 @@ const axios = require("axios");
 const apiKey = process.env.BOOKS_API_KEY;
 
 
-router.get("/web/book/:id", async (req, res, next) => {
+router.get("/books/", async (req, res, next) => {
   try {
-    const query = req.params.id;
-    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`);
-    console.log(response);
+    const { q } = req.query;
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${q}&projection=lite&key=${apiKey}`);
+    res.json(response.data);
+  }
+  catch (err) {
+    console.error("Error in API Call", err);
+  }
+})
+
+router.get("/book/", async (req, res, next) => {
+  try {
+    const { selfLink } = req.query;
+    const response = await axios.get(`${selfLink}?projection=lite&key=${apiKey}`);
     res.json(response.data);
   }
   catch (err) {
