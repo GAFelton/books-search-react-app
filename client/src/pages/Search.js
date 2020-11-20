456b836263b51f3ee5852fd150e08de224b67c78
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-20 14:18:27 -0800

    Moved API call and book handling from App.js to Search.js.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,47 @@
 import React, { useState } from "react";
-import { BookList, BookListItem } from "./components/BookList";
-import { Container, Row, Col } from "./components/Grid";
-import Input from "./components/Input";
-import Button from "./components/Button";
+import { BookList, BookListItem } from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import API from "../utils/API";
 
 function Search() {
+  const [books, setBooks] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { value } = event.target;
+    setBookSearch(value);
+  };
+  const interpretData = (data) => {
+    const output = [];
+    data.map(book => {
+      const bookObj = {
+        id: book.id,
+        selfLink: book.selfLink,
+        title: book.volumeInfo.title,
+        subtitle: book.volumeInfo.subtitle,
+        authors: [book.volumeInfo.authors],
+        description: book.volumeInfo.description,
+        publisher: book.volumeInfo.publisher,
+        publishedDate: book.volumeInfo.publishedDate,
+        previewLink: book.volumeInfo.previewLink,
+        image: book.volumeInfo.imageLinks.thumbnail,
+      };
+      output.push(bookObj);
+    })
+    setBooks(output);
+  }
+  const handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get books update the books state
+    event.preventDefault();
+    API.web.get(bookSearch)
+      .then(res => interpretData(res.data.items))
+      .catch(err => console.log(err));
+  };
+
   return (
     <Container>
       <Row>
@@ -45,7 +82,7 @@ function Search() {
                     <BookListItem
                       key={book.title}
                       title={book.title}
-                      href={book.href}
+                      href={book.link}
                       ingredients={book.ingredients}
                       thumbnail={book.thumbnail}
                     />
