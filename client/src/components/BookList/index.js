beb094602b5029a1d0828bb5f1c5055eddb982ab
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-20 21:18:19 -0800

    Updated syntax to react-bootstrap, added checkbox prop.

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Thumbnail from "../Thumbnail";
-import { Container, Row, Col } from "../Grid";
-import { ListGroup } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, Form } from "react-bootstrap";
 
 // Exporting both BookList and BookListItem from this file
 
@@ -16,16 +15,20 @@ export function BookListItem({
   description,
   image,
   link,
-  title
+  title,
+  checkbox
 }) {
   return (
     <ListGroup.Item>
       <Container>
         <Row>
-          <Col size="xs-4 sm-2">
+        <Col xs={1}>
+          <Form.Check aria-label={`Save option: ${title}`} onClick={checkbox} />
+        </Col>
+          <Col xs={4} sm={2}>
             <Thumbnail src={image || "https://placehold.it/300x300"} />
           </Col>
-          <Col size="xs-8 sm-9">
+          <Col xs={8} sm={9}>
             <h3>{title}</h3>
             <p>Description: {description}</p>
             <p>By: {authors}</p>
