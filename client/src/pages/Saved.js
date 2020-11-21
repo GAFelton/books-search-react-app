8312639296fa6b2cf4568df6848b685ca4260cb8
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-20 17:24:45 -0800

    Added loadBooks useEffect() to pull from db.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 function Saved() {
+  const [books, setBooks] = useState([]);
 
+  useEffect(async () => {
+    loadBooks();
+  })
+
+  function loadBooks() {
+    try {
+      const response = await API.server.searchDB();
+      setBooks(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <></>
+    <>
+
+    </>
   );
 };
 
