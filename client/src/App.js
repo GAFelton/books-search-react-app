f572aef0115282a75cd8e6ac34c19a36f0b2e2b6
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-20 21:17:30 -0800

    Implmented React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { Container } from "react-bootstrap";
 import HeroSection from "./components/HeroSection";
 import Header from "./components/Header";
 import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 
 function App() {
 
 
   return (
-    <div>
-      <Header />
-      <HeroSection />
-      <Search />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Container fluid>
+          <Switch>
+            <Route path="/" exact>
+              <HeroSection />
+              <Search />
+            </Route>
+            <Route path="/search">
+              <HeroSection />
+              <Search />
+            </Route>
+            <Route path="/saved">
+              <Saved />
+            </Route>
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
