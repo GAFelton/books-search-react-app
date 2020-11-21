8819308050441e7e05a826a58f60ed048e597d6d
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-21 00:20:56 -0800

    Filled out bookDisplayArea and handleBookLinkClick.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,50 +1,76 @@
 import React, { useState, useEffect } from "react";
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { BookListItem } from "../components/BookList";
 import DeleteBtn from "../components/DeleteBtn";
 import API from "../utils/API";
 
 function Saved() {
   const [books, setBooks] = useState([]);
+  const [displayBook, setDisplayBook] = useState({});
 
   useEffect(async () => {
-    loadBooks();
+    await loadBooks();
   }, []);
 
   async function loadBooks() {
     try {
       const response = await API.server.searchDB();
-      setBooks(response);
+      await setBooks(response.data);
+      await setDisplayBook(books[0]);
     } catch (error) {
       console.log(error);
     }
   };
 
-    // Deletes a book from the database with a given id, then reloads books from the db
-    function deleteBook(id) {
-      API.server.delete(id)
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
+  // Deletes a book from the database with a given id, then reloads books from the db
+  function deleteBook(id) {
+    API.server.delete(id)
+      .then(res => loadBooks())
+      .catch(err => console.log(err));
+  }
 
-    function handleBookLinkClick() {
+  function findBookInState(id) {
+    return books.filter(item => {
+      return item.key === id;
+    });
+  };
 
-    }
+  function handleBookLinkClick(key) {
+    const bookToDisplay = findBookInState(key);
+    setDisplayBook(bookToDisplay);
+  }
 
   return (
     <>
       <Container>
         <Row>
-          <Col className="bookDisplayArea">
+          <Col xs={9} className="bookDisplayArea">
+            {displayBook ? (
+              <div>
+                <h1>This Book: {displayBook.title}</h1>
+                <BookListItem
+                  key={displayBook.key}
+                  title={displayBook.title}
+                  authors={displayBook.authors}
+                  link={displayBook.previewLink}
+                  description={displayBook.description}
+                  image={displayBook.image}
+                />
+              </div>
+            ) : (
+                " "
+              )}
           </Col>
-          <Col className="sideboard">
+          <Col xs={3} className="sideboard">
+            <h2>Saved Books:</h2>
             {books.length ? (
               <ListGroup>
                 {books.map(book => {
                   return (
                     <ListGroup.Item key={book._id}>
-                      <a onClick={handleBookLinkClick}>
+                      <a onClick={() => handleBookLinkClick(book.key)}>
                         <strong>
-                          {book.title} by {book.authors[0]}
+                          {book.title} by {book.authors}
                         </strong>
                       </a>
                       <DeleteBtn onClick={() => deleteBook(book._id)} />
