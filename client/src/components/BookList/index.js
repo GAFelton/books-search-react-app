c7e4a5065b292828eed6718faa4c7295036775f6
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-22 17:03:01 -0800

    Added conditional formatting to BookListItem, to add more information on Saved Books page.

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -16,24 +16,33 @@ export function BookListItem({
   image,
   link,
   title,
-  checkbox
+  checkbox,
+  read,
+  checkText,
+  subtitle,
+  publisher,
+  publishedDate
 }) {
   return (
     <ListGroup.Item>
       <Container>
         <Row>
-          {checkbox ? (
-            <Col xs={1}>
-            <Form.Check aria-label={`Save option: ${title}`} onClick={checkbox} />
-            </Col>
-          ) : (
-            " "
-          )}
           <Col xs={4} sm={2}>
             <Thumbnail src={image || "https://placehold.it/300x300"} />
+            {checkbox ? (
+              <Form>
+                <Form.Text muted>{checkText}:</Form.Text>
+                <Form.Check aria-label={`${checkText} book: ${title}`} onChange={checkbox} checked={read ? !!read : undefined} />
+              </Form>
+            ) : (
+                " "
+              )}
           </Col>
           <Col xs={8} sm={9}>
-            <h3>{title}</h3>
+            <h3 className="text-center">{title}</h3>
+            {subtitle ? (
+              <h4 className="text-center">{subtitle}</h4>
+            ) : ( " " )}
             <p>Description: {description}</p>
             <p>By: {authors}</p>
             <a rel="noreferrer noopener" target="_blank" href={link}>
@@ -41,6 +50,18 @@ export function BookListItem({
             </a>
           </Col>
         </Row>
+        {publisher ? (
+          <Row className="mt-1">
+            <Col>
+              <h5>Publisher: {publisher}</h5>
+            </Col>
+            {publishedDate ? (
+              <Col>
+              <h5>First Published: {publishedDate}</h5>
+                </Col>
+            ) : ( " " )}
+          </Row>
+        ) : ( " " )}
       </Container>
     </ListGroup.Item>
   );
